ab61c7c9c2093097f223a90334a5e15c6996989a
sarmadkhansb <great.sarmad8@gmail.com>
2023-10-31 23:08:59 +0500

    updated design of ideas list

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -4,7 +4,6 @@ import { Auth } from 'aws-amplify';
 
 const HomeScreen = ({ navigation }) => {
   const [isHeadingClicked, setHeadingClicked] = useState(false);
-  const [showIdeaBoxes, setShowIdeaBoxes] = useState(false);
   const [scrollAnim] = useState(new Animated.Value(0));
   const [typingText, setTypingText] = useState('');
   const [typingIndex, setTypingIndex] = useState(0);
@@ -32,10 +31,7 @@ const HomeScreen = ({ navigation }) => {
   }, [isHeadingClicked, typingIndex]);
 
   const handleHeadingClick = () => {
-    if (!showIdeaBoxes) {
-      setHeadingClicked(!isHeadingClicked);
-      setShowIdeaBoxes(true);
-    }
+    setHeadingClicked(!isHeadingClicked);
   };
 
   const signOut = async () => {
@@ -53,24 +49,45 @@ const HomeScreen = ({ navigation }) => {
           style={styles.scrollHeader}
           onPress={() => handleHeadingClick()}
         >
-          <Text style={styles.scrollHeaderText}>
-            {isHeadingClicked ? 'We will get you ' : 'Explore Business Ideas'}
+          <Text style={[styles.scrollHeaderText, isHeadingClicked && styles.clickedText]}>
+            {isHeadingClicked ? 'We will get you Jee ' : 'Explore Business Ideas'}
           </Text>
         </TouchableOpacity>
         <Animated.View
           style={[
             styles.scrollContent,
             isHeadingClicked && styles.scrolledContent,
-            { opacity: scrollAnim.interpolate({ inputRange: [0, 1], outputRange: [1, 0] }) }
+            { opacity: scrollAnim.interpolate({ inputRange: [0, 1], outputRange: [1, 0] }) },
           ]}
         >
-          <Text style={styles.scrollText}>
-            {"\n"}
-            <Text style={styles.glowText}>Choose your Idea</Text>{"\n"}
-            <Text style={styles.glowText}>Get funding</Text>{"\n"}
-            <Text style={styles.glowText}>Start your Business</Text>{"\n"}
-            <Text style={styles.glowText}>Market your Business</Text>{"\n"}
-          </Text>
+          {isHeadingClicked ? (
+            <>
+              <TouchableOpacity
+                onPress={() => {}}
+                style={styles.bouncyText}
+              >
+                <Text style={styles.buttonText}>Choose your Idea</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {}}
+                style={styles.bouncyText}
+              >
+                <Text style={styles.buttonText}>Get funding</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {}}
+                style={styles.bouncyText}
+              >
+                <Text style={styles.buttonText}>Start your Business</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {}}
+                style={styles.bouncyText}
+              >
+                <Text style={styles.buttonText}>Market your Business</Text>
+              </TouchableOpacity>
+            </>
+          ) : null}
         </Animated.View>
         <Text style={styles.singleLineText}>{typingText}</Text>
       </View>
@@ -82,7 +99,7 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5', // New background color
+    backgroundColor: '#F5F5F5',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -102,31 +119,33 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: '#B069FF',
-    fontFamily: 'Verdana',
+    fontFamily: 'Helvetica, Arial, sans-serif',
+  },
+  clickedText: {
+    transform: [{ scale: 1.1 }], // Slight bounce effect
   },
   scrollContent: {
-    marginTop: 20, // Increased distance between lines
+    marginTop: 20,
     height: 0,
-    alignItems: 'center', // Centered text
+    alignItems: 'center',
   },
   scrolledContent: {
     height: 'auto',
   },
-  scrollText: {
+  bouncyText: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  buttonText: {
     fontSize: 16,
     color: '#B069FF',
     fontFamily: 'Verdana',
   },
-  glowText: {
-    textShadowColor: 'rgba(176, 105, 255, 0.7)',
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 10,
-  },
   singleLineText: {
-    fontSize: 14, // Reduced size for the typing line
+    fontSize: 14,
     color: '#B069FF',
     fontFamily: 'Verdana',
-    marginTop: 20, // Increased distance
+    marginTop: 20,
     marginLeft: 40,
     alignItems: 'center',
   },
